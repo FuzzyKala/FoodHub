efac7ead22c8ac7e0808038919cfbaa77c9c1671
FuzzyKala <tangerinekey381@gmail.com>
2024-03-25 21:14:39 +0200

    updated .env file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require(cors);
 const { Pool } = require("pg");
@@ -5,6 +6,6 @@ const { Pool } = require("pg");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 3001;
+const port = process.env.PORT;
 
 app.listen(port, console.log(`Port:${port} has been listening....`));
