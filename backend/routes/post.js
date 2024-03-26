f18bc2c8f060369090ee479b6766f21dbd674d66
FuzzyKala <tangerinekey381@gmail.com>
2024-03-26 22:26:57 +0200

    add a new post function ready (photo included)

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,7 +2,10 @@ const express = require("express");
 const { query } = require("../query/db");
 const postRouter = express.Router();
 const moment = require("moment-timezone");
+const multer = require("multer");
+const upload = multer();
 
+// get all posts
 postRouter.get("/", async (req, res) => {
   // console.log(query);
   try {
@@ -16,13 +19,33 @@ postRouter.get("/", async (req, res) => {
   }
 });
 
-postRouter.post("/new", async (req, res) => {
+// get a specific post by id
+postRouter.get("/:post_id", async (req, res) => {
+  const post_id = Number(req.params.post_id);
+  try {
+    const posts = await query("select * from post where post_id = $1", [
+      post_id,
+    ]);
+    // this is for empty database checking.
+    const rows = posts.rows ? posts.rows : [];
+    res.status(200).json(rows);
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).json({ error: err });
+  }
+});
+
+// add a new post
+postRouter.post("/new", upload.single("photo"), async (req, res) => {
+  console.log(req.file);
   try {
     // need to get a time zone from frontend cookie I guess??
     const timestamp = moment().tz("Europe/Berlin").format();
+    // Extract photo data from request
+    const photoData = req.file ? req.file.buffer : null;
     const result = await query(
-      "insert into post (account_id, description, date) values ($1, $2, $3) returning *",
-      [req.body.account_id, req.body.description, timestamp]
+      "insert into post (account_id, description, photo_data, date) values ($1, $2, $3, $4) returning *",
+      [req.body.account_id, req.body.description, photoData, timestamp]
     );
     // res.status(200).json({ id: result.rows[0].accound_id });
     res.status(200).json(result.rows[0]);
