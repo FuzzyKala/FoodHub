14400e21136558fc631932d4434a7b6be2cd8618
FuzzyKala <tangerinekey381@gmail.com>
2024-03-26 17:00:13 +0200

    get all posts ready

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,3 +1,20 @@
 const express = require("express");
-// const { query } = require("../helpers/db");
+const { query } = require("../query/db");
 const postRouter = express.Router();
+
+postRouter.get("/", async (req, res) => {
+  // console.log(query);
+  try {
+    const posts = await query("select * from post");
+    // this is for empty database checking.
+    const rows = posts.rows ? posts.rows : [];
+    res.status(200).json(rows);
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).json({ error: err });
+  }
+});
+
+module.exports = {
+  postRouter,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
+const { postRouter } = require("./routes/post");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", postRouter);
 const port = process.env.PORT;
 
 app.listen(port, console.log(`Port:${port} has been listening....`));
