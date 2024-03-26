761e42cbf029e96d643855b46324c1210ea345b5
FuzzyKala <tangerinekey381@gmail.com>
2024-03-26 21:11:53 +0200

    add a new post function ready (photo not yet)

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { query } = require("../query/db");
 const postRouter = express.Router();
+const moment = require("moment-timezone");
 
 postRouter.get("/", async (req, res) => {
   // console.log(query);
@@ -12,6 +13,22 @@ postRouter.get("/", async (req, res) => {
   } catch (err) {
     res.statusMessage = err;
     res.status(500).json({ error: err });
+  }
+});
+
+postRouter.post("/new", async (req, res) => {
+  try {
+    // need to get a time zone from frontend cookie I guess??
+    const timestamp = moment().tz("Europe/Berlin").format();
+    const result = await query(
+      "insert into post (account_id, description, date) values ($1, $2, $3) returning *",
+      [req.body.account_id, req.body.description, timestamp]
+    );
+    // res.status(200).json({ id: result.rows[0].accound_id });
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    // res.status(500).json({ error: err.message });
+    res.status(500).json(err);
   }
 });
 
