3727f57d036f1ba339089c2fe92c65bf17840397
FuzzyKala <tangerinekey381@gmail.com>
2024-03-30 11:55:55 +0200

    Update environment variables and database connection in backend code

--- backend/query/db.js
+++ backend/query/db.js
@@ -1,11 +1,42 @@
 require("dotenv").config();
-const { Client } = require("pg");
-// const { Pool } = require("pg");
+// const { Client } = require("pg");
+const { Pool } = require("pg");
 // const fs = require("fs");
 // const path = require("path");
 
-// Connection config
+// // Connection config
+// const config = {
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PW,
+//   port: process.env.DB_PORT,
+//   ssl: true,
+// };
 
+// // Connect to the database
+// const client = new Client(config);
+// client
+//   .connect()
+//   .then(() => {
+//     console.log("Connected to PostgreSQL database - db.js");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to PostgreSQL database:", err);
+//   });
+
+// // Define a function to execute queries
+// const query = async (sql, values = []) => {
+//   try {
+//     const result = await client.query(sql, values);
+
+//     return result;
+//   } catch (error) {
+//     throw new Error(error.message);
+//   }
+// };
+
+// // Connection config
 const config = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -15,23 +46,12 @@ const config = {
   ssl: true,
 };
 
-const client = new Client(config);
-
-// Connect to the database
-client
-  .connect()
-  .then(() => {
-    console.log("Connected to PostgreSQL database - db.js");
-  })
-  .catch((err) => {
-    console.error("Error connecting to PostgreSQL database:", err);
-  });
+const pool = new Pool(config);
 
 // Define a function to execute queries
 const query = async (sql, values = []) => {
   try {
-    const result = await client.query(sql, values);
-
+    const result = await pool.query(sql, values);
     return result;
   } catch (error) {
     throw new Error(error.message);
