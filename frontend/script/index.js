db5e79847054274657b4b41019778fffde9f2e12
FuzzyKala <tangerinekey381@gmail.com>
2024-04-01 14:14:57 +0300

    Update frontend UI and backend URL

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,23 +1,27 @@
-const backendUrl = "food-hub-oamk.vercel.app";
-// const backendUrl = 'http://localhost:10000/2'
+// const backendUrl = "food-hub-oamk.vercel.app";
+const backendUrl = `http://localhost:10000`;
 
 // for rendering photo testing
 const renderPost = async () => {
-  const response = await fetch("backendUrl");
+  const response = await fetch(`${backendUrl}/posts`);
   const json = await response.json();
+  const photoBuffer = json[8].photo_data;
 
-  const photoData = json.photo_data.data;
-  const uint8Array = new Uint8Array(photoData);
-  const blob = new Blob([uint8Array], { type: "image/jpeg" });
+  if (photoBuffer != null) {
+    const photo = photoBuffer.data;
 
-  // Create a data URL from the Blob
-  const reader = new FileReader();
-  reader.onload = function (event) {
-    // Set the data URL as the src attribute of the img tag
-    const img = document.querySelector("img");
-    img.src = event.target.result;
-  };
-  reader.readAsDataURL(blob);
+    const uint8Array = new Uint8Array(photo);
+    const blob = new Blob([uint8Array], { type: "image/jpeg" });
+
+    // Create a data URL from the Blob
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      // Set the data URL as the src attribute of the img tag
+      const img = document.querySelector("img");
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(blob);
+  }
 };
 
 renderPost();
