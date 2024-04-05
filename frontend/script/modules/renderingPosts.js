f34db3c53e019dc775c48a836f7b6cc60d738bb7
FuzzyKala <tangerinekey381@gmail.com>
2024-04-05 23:44:58 +0300

    Refactor renderingPost and getPosts functions in frontend script

--- frontend/script/modules/renderingPosts.js
+++ frontend/script/modules/renderingPosts.js
@@ -1,11 +1,26 @@
-export const renderingPost = async (trendingPosts) => {
-  const postList = await trendingPosts.getPosts();
-  console.log(postList);
-  renderingPhoto(postList);
-  renderingText(postList);
+// export const renderingPost = async (trendingPosts) => {
+//   const postList = await trendingPosts.getPosts();
+//   console.log(postList);
+//   renderingPhoto(postList);
+//   renderingText(postList);
 
-  // keep this for comment system later
-  const commentNum = document.getElementById("commentNum");
+//   // keep this for comment system later
+//   const commentNum = document.getElementById("commentNum");
+// };
+
+export const renderingPost = async (trendingPosts) => {
+  try {
+    const postList = await trendingPosts.getPosts();
+    console.log(postList);
+    if (postList.length === 0) {
+      console.log("No trending posts found.");
+      return;
+    }
+    renderingPhoto(postList);
+    renderingText(postList);
+  } catch (error) {
+    console.error("Error rendering posts:", error);
+  }
 };
 
 const renderingPhoto = (postList) => {
@@ -29,6 +44,27 @@ const renderingPhoto = (postList) => {
     });
   }
 };
+// const renderingPhoto = (postList) => {
+//   const photoContainer = document.getElementById("photoContainer");
+//   photoContainer.innerHTML = "";
+//   if (postList != null) {
+//     postList.forEach((post, i) => {
+//       const photo = post.getPhotoData();
+//       const uint8Array = new Uint8Array(photo);
+//       const blob = new Blob([uint8Array], { type: "image/*" });
+//       console.log("Created Blob:", blob); // Log created Blob object
+//       const url = URL.createObjectURL(blob);
+//       // console.log(url);
+//       const img = document.createElement("img");
+//       img.src = url;
+//       img.alt = "photo";
+//       img.height = "auto";
+//       img.width = "100%";
+//       img.className = "img-fluid rounded mx-auto d-block mb-3";
+//       photoContainer.appendChild(img);
+//     });
+//   }
+// };
 
 const renderingText = (postList) => {
   const username = document.getElementById("username");
--- frontend/script/modules/trending.js
+++ frontend/script/modules/trending.js
@@ -2,27 +2,66 @@ import { Post } from "./post.js";
 class Trending {
   #posts = [];
   #backendUrl = "";
+
   constructor(url) {
     this.#backendUrl = url;
   }
+
+  // getPosts = async () => {
+  //   try {
+  //   } catch (error) {}
+  //   return new Promise(async (resolve, reject) => {
+  //     fetch(this.#backendUrl + "/posts/trending")
+  //       .then((res) => {
+  //         return res.json();
+  //       })
+  //       .then(
+  //         (json) => {
+  //           this.#readJson(json);
+  //           resolve(this.#posts);
+  //         },
+  //         (err) => {
+  //           reject(err);
+  //         }
+  //       );
+  //   });
+  // };
   getPosts = async () => {
-    return new Promise(async (resolve, reject) => {
-      fetch(this.#backendUrl + "/posts/trending")
-        .then((res) => {
-          return res.json();
-        })
-        .then(
-          (json) => {
-            this.#readJson(json);
-            resolve(this.#posts);
-          },
-          (err) => {
-            reject(err);
-          }
+    try {
+      const response = await fetch(this.#backendUrl + "/posts/trending");
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch trending posts: ${response.statusText}`
         );
-    });
+      }
+      const json = await response.json();
+      this.#readJson(json);
+      return this.#posts;
+    } catch (error) {
+      console.error("Error fetching trending posts:", error);
+      return [];
+    }
   };
+
+  //   #readJson = (json) => {
+  //     this.#posts = [];
+  //     json.forEach((post) => {
+  //       const newPost = new Post(
+  //         post.post_id,
+  //         post.username,
+  //         post.description,
+  //         post.photo_data,
+  //         post.rate,
+  //         post.comment_num,
+  //         post.date
+  //       );
+  //       this.#posts.push(newPost);
+  //     });
+  //   };
+  // }
+
   #readJson = (json) => {
+    console.log("Received JSON data:", json); // Log received JSON data
     this.#posts = [];
     json.forEach((post) => {
       const newPost = new Post(
@@ -34,6 +73,7 @@ class Trending {
         post.comment_num,
         post.date
       );
+      console.log("Created Post:", newPost); // Log created Post object
       this.#posts.push(newPost);
     });
   };
