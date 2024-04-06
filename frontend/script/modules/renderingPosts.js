46723e877db597ae36edd0f6f226d033379ebf6d
FuzzyKala <tangerinekey381@gmail.com>
2024-04-06 19:51:47 +0300

    Refactor renderingPost function in frontend script to render only the first photo

--- frontend/script/modules/renderingPosts.js
+++ frontend/script/modules/renderingPosts.js
@@ -1,58 +1,3 @@
-// export const renderingPost = async (trendingPosts) => {
-//   const postList = await trendingPosts.getPosts();
-//   renderingPhoto(postList);
-//   renderingText(postList);
-
-//   // keep this for comment system later
-//   const commentNum = document.getElementById("commentNum");
-// };
-
-// const renderingPhoto = (postList) => {
-//   const postContainer = document.getElementById("postContainer");
-//   postContainer.innerHTML = "";
-//   if (postList != null) {
-//     postList.forEach((post) => {
-//       const photos = post.getPhotoData();
-//       console.log("photo:", photos);
-
-//       photos.forEach((photo) => {
-//         const photoData = photo.data;
-//         const uint8Array = new Uint8Array(photoData);
-//         const blob = new Blob([uint8Array], { type: "image/jpeg" });
-//         const url = URL.createObjectURL(blob);
-//         // console.log(url);
-//         const div = document.createElement("div");
-//         div.className = "carousel-item";
-//         div.style.height = "400px";
-//         div.style.width = "100%";
-//         div.style.overflow = "hidden";
-//         div.style.position = "relative";
-
-//         const img = document.createElement("img");
-//         img.src = url;
-//         img.alt = "photo";
-//         img.height = "auto";
-//         img.width = "100%";
-//         img.className = "img-fluid rounded mx-auto d-block mb-3";
-
-//         div.appendChild(img);
-//         postContainer.appendChild(div);
-//       });
-//     });
-//   }
-// };
-
-// const renderingText = (postList) => {
-//   const username = document.getElementById("username");
-//   const description = document.getElementById("description");
-//   const rate = document.getElementById("rate");
-//   const date = document.getElementById("date");
-//   username.textContent = postList[0].getUsername();
-//   description.textContent = postList[0].getDescription();
-//   rate.textContent = postList[0].getRate();
-//   date.textContent = postList[0].getDate();
-// };
-
 export const renderingPost = async (trendingPosts) => {
   const postList = await trendingPosts.getPosts();
   renderingPostContainer(postList);
@@ -73,15 +18,18 @@ const createPostItem = (post, isActive) => {
   const postItem = document.createElement("div");
   postItem.className = isActive ? "carousel-item active" : "carousel-item";
   const photos = post.getPhotoData();
-  photos.forEach((photo) => {
-    const img = createImage(photo);
-    postItem.appendChild(img);
-  });
+  // rendering first photo only
+  const photo = photos[0];
+  const img = createImage(photo);
+  postItem.appendChild(img);
+  // photos.forEach((photo) => {
+  //   const img = createImage(photo);
+  //   postItem.appendChild(img);
+  // });
   return postItem;
 };
 
 const createImage = (photo) => {
-  console.log(photo);
   const photoData = photo.data;
   const uint8Array = new Uint8Array(photoData);
   const blob = new Blob([uint8Array], { type: "image/jpeg" });
