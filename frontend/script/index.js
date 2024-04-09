f19564be456ad692e11789f2f219434b5b75f88b
FuzzyKala <tangerinekey381@gmail.com>
2024-04-09 18:53:28 +0300

    Refactor frontend script to update backend URL and enable local development

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -3,8 +3,8 @@ import { registration, login, addNewPost } from "./modules/eventHandling.js";
 import { Trending } from "./modules/trending.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const backendUrl = "food-hub-oamk.vercel.app";
-  // const backendUrl = `http://localhost:10000`;
+  // const backendUrl = "food-hub-oamk.vercel.app";
+  const backendUrl = `http://localhost:10000`;
 
   const trending = new Trending(backendUrl);
   renderingPost(trending);
