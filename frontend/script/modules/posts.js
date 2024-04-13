5eb22e91ffc13b0a437f9c5ce05fd97c3d5db604
FuzzyKala <tangerinekey381@gmail.com>
2024-04-13 11:45:37 +0300

    Refactor getPosts method to use async/await and handle errors in posts.js

--- frontend/script/modules/posts.js
+++ frontend/script/modules/posts.js
@@ -11,40 +11,35 @@ class posts {
 
   getPosts = async () => {
     try {
-    } catch (error) {}
-    return new Promise(async (resolve, reject) => {
-      fetch(this.#backendUrl + this.#restUrl)
-        .then((res) => {
-          return res.json();
-        })
-        .then(
-          (json) => {
-            this.#readJson(json);
-            resolve(this.#posts);
-          },
-          (err) => {
-            reject(err);
-          }
-        );
-    });
+      const response = await fetch(`${this.#backendUrl}${this.#restUrl}`);
+      if (response.ok) {
+        const json = await response.json();
+        this.#readJson(json);
+        return this.#posts;
+      } else {
+        throw new Error("Failed to fetch posts");
+      }
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      return [];
+    }
   };
 
   #readJson = (json) => {
-    this.#posts = [];
-    json.forEach((post) => {
-      const newPost = new Post(
-        post.post_id,
-        post.username,
-        post.title,
-        post.description,
-        post.category,
-        post.photo_data,
-        post.rate,
-        post.comment_num,
-        post.date
-      );
-      this.#posts.push(newPost);
-    });
+    this.#posts = json.map(
+      (post) =>
+        new Post(
+          post.post_id,
+          post.username,
+          post.title,
+          post.description,
+          post.category,
+          post.photo_data,
+          post.rate,
+          post.comment_num,
+          post.date
+        )
+    );
   };
 }
 
